2b328122c124cd01bda2676227600ad95f06ca7e
Sam Dollar <sdollar1@gmail.com>
2021-08-02 17:33:22 -0500

    first working version

--- script.js
+++ script.js
@@ -1,3 +1,40 @@
-const sixteen = document.getElementById('16');
-const thirtyTwo = document.getElementById('32');
-const sixtyFour = document.getElementById('64');
+const sixteen = document.getElementById('sixteen');
+const thirtyTwo = document.getElementById('thirtytwo');
+const sixtyFour = document.getElementById('sixtyfour');
+const drawpad = document.getElementById('drawpad');
+
+createGrid(16);
+
+sixteen.addEventListener('click', function() {
+    clearGrid();
+    createGrid(16);
+})
+
+thirtyTwo.addEventListener('click', function() {
+    clearGrid();
+    createGrid(32);
+})
+
+sixtyFour.addEventListener('click', function() {
+    clearGrid();
+    createGrid(64);
+})
+
+function createGrid(items) {
+    drawpad.style.setProperty('--grid-rows', items);
+    drawpad.style.setProperty('--grid-cols', items);
+    for(let i = 0; i < (items * items); i++) {
+        let cell = document.createElement('div');
+        cell.addEventListener('mouseover', function() {
+            cell.style.backgroundColor = 'black';
+        })
+        drawpad.appendChild(cell).className = 'cell';
+    }
+    
+}
+
+function clearGrid() {
+    while (drawpad.firstChild){
+        drawpad.removeChild(drawpad.firstChild);
+    }
+}
